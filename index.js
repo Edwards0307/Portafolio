896d655b04424740e4c2932efdb8c4c13b6bcd87
Edwards Ardila <edwards.ardila03@gmail.com>
2023-03-19 17:49:37 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  //oculto el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+
+//Detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function () {
+  efectoHabilidades();
+};
